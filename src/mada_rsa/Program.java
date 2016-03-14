0e4cc3d37418d8b7914d3d2232176d2c85b75893
= <patric.steiner@students.fhnw.ch>
2016-03-14 08:53:58 +0100

    made program more verbose

--- src/mada_rsa/Program.java
+++ src/mada_rsa/Program.java
@@ -25,46 +25,57 @@ public class Program {
 	 * Change the paths above if you want something else.
 	 */
     public static void main(String[] args) {
-//    	task1();
+    	task1();
 //    	task2();
 //    	task3();
-    	task4();
+//    	task4();
     }
     
     /**
-     * Task #1: Generate random public and private key, save to sk.txt and pk.txt respectively
+     * Task #1: Generate random public and private key, save to sk.txt and pk.txt respectively.
      */
     public static void task1() {
     	 KeyPair keyPair = new KeyPair();
          Util.writeToFile(PATH_PRIVATE_KEY, keyPair.getPrivateKey().toString());
          Util.writeToFile(PATH_PUBLIC_KEY, keyPair.getPublicKey().toString());
+         System.out.println("Generated new Keypair, saved private key to " + PATH_PRIVATE_KEY + " and public key to " + PATH_PUBLIC_KEY);
+         System.out.println("Private key: " + keyPair.getPrivateKey());
+         System.out.println("Public key: " + keyPair.getPublicKey());
     }
     
     /**
-     * Read text from file text.txt and encrypt it with the public key in pk.txt.
-     * write output to chiffre.txt.
+     * Task #2: Read text from file text.txt and encrypt it with the public key in pk.txt.
+     * Write output to chiffre.txt.
      */
     public static void task2() {
     	PublicKey publicKey = new PublicKey(PATH_PUBLIC_KEY);
     	String message = Util.readFromFile(PATH_MESSAGE);
-    	Util.writeToFile(PATH_ENCRYPTED_MESSAGE, RSA.encrypt(message, publicKey));
+    	String encryptedMessage = RSA.encrypt(message, publicKey);
+    	Util.writeToFile(PATH_ENCRYPTED_MESSAGE, encryptedMessage);
+    	System.out.println("Message from " + PATH_MESSAGE + " : " + message);
+    	System.out.println("Encrypted message saved to " + PATH_ENCRYPTED_MESSAGE + ": " + encryptedMessage);
     }
     
     /**
-     * Use private key in sk.txt to decrypt the text in chiffre.txt.
+     * Task #3: Private key in sk.txt to decrypt the text in chiffre.txt.
      * Write output to text-d.txt.
      */
     public static void task3() {
+        System.out.println("Be patient, decrypting may take a few seconds...");
     	PrivateKey privateKey = new PrivateKey(PATH_PRIVATE_KEY);
     	String encryptedMessage = Util.readFromFile(PATH_ENCRYPTED_MESSAGE);
-    	Util.writeToFile("text-d.txt", RSA.decrypt(encryptedMessage, privateKey));
+    	String decryptedMessage = RSA.decrypt(encryptedMessage, privateKey);
+    	Util.writeToFile("text-d.txt", decryptedMessage);
+    	System.out.println("Encrypted message from " + PATH_ENCRYPTED_MESSAGE + ": " + encryptedMessage);
+    	System.out.println("Decrypted message saved to " + PATH_DECRYPTED_MESSAGE + ": " + decryptedMessage);
     }
     
     /**
-     * Decrypt text in chiffre.txt with private key in sk.txt.
+     * Task #4: Decrypt text in chiffre.txt with private key in sk.txt.
      * Output decrypted message to stdout.
      */
     public static void task4() {
+        System.out.println("Be patient, decrypting may take a few seconds...");
     	PrivateKey privateKey = new PrivateKey(PATH_PRIVATE_KEY);
     	String encryptedMessage = Util.readFromFile(PATH_ENCRYPTED_MESSAGE);
     	System.out.println(RSA.decrypt(encryptedMessage, privateKey));
